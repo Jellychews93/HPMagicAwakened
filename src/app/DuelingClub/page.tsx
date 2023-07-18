92f87fc5a98c95b17d9b36cc02aeb565b726650f
Johnnam1121 <johnnam93@gmail.com>
2023-07-18 14:54:50 -0500

    finished footer and dueling club

--- src/app/DuelingClub/page.tsx
+++ src/app/DuelingClub/page.tsx
@@ -5,6 +5,9 @@ import React from 'react'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Image from 'next/image';
 import SoloorDuo from '../../../public/assets/Duel/SoloorDuo.jpg'
+import Ranking from '../../../public/assets/Duel/Ranking.jpg'
+import Selection from '../../../public/assets/Duel/Selection.jpg'
+import Footer from '@/components/Footer';
 
 const theme = createTheme({
   palette: {
@@ -69,12 +72,56 @@ function page() {
       <Typography variant='h4' sx={{ textAlign: 'center', mx: '5vw', mb: '3vh' }}>
         The Dueling Club is HPMA versions of PVP. You can either participate in 1v1 or 2v2.
       </Typography>
-      <Image width={400} height={400} src={SoloorDuo.src} alt='Solo or Duo' style={{ marginInline: 'auto' }} />
+      <Grid container spacing={4}>
+        <Grid xs={12} md={2} />
+        <Grid item xs={12} md={4}>
+          <Image width={400} height={400} src={SoloorDuo.src} alt='Solo or Duo' style={{ marginInline: 'auto' }} />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Typography variant='h3'>Solo or Duo</Typography>
+          <Typography variant='h4'>You can either play solo in 1v1 or with a parter in 2v2</Typography>
+        </Grid>
+        <Grid xs={12} md={2} />
+        <Grid xs={12} md={2} />
+        <Grid item xs={12} md={4}>
+          <Image width={400} height={400} src={Ranking.src} alt='Ranking up' style={{ marginInline: 'auto' }} />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Typography variant='h3'>Ranking Up</Typography>
+          <Typography variant='h4'>In both solo and duos by winning, you gain points. After reaching certain point thresholds, you rank up! It goes in this order,<br />
+            Novice, Bronze, Silver, Gold, Platinum, Diamond, Grand Master, Magic Awakened</Typography>
+        </Grid>
+        <Grid xs={12} md={2} />
+      </Grid>
+
+      <Typography variant='h1' sx={{ textAlign: 'center', my: '3vh' }}>
+        Grand Master Dueling Club
+      </Typography>
+      <Typography variant='h4' sx={{ textAlign: 'center', mx: '5vw', mb: '3vh' }}>
+        What is it?
+      </Typography>
+      <Typography variant='h4' sx={{ textAlign: 'center', mx: '5vw', mb: '3vh' }}>
+        The GM Dueling Club is for those who make it into Grand Masters and reach a certain predetermined rating. For this season in 2023,
+        it is 8100. If you reach that point, you will be invited into the GM Dueling Club.
+      </Typography>
+      <Grid container spacing={4}>
+        <Grid xs={12} md={2} />
+        <Grid item xs={12} md={4}>
+          <Image width={400} height={400} src={Selection.src} alt='Solo or Duo' style={{ marginInline: 'auto' }} />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Typography variant='h3'>Deck Selection</Typography>
+          <Typography variant='h4'>
+            In the GM Dueling Club, you are required to make 3 decks. After you match with someone, each player will ban one deck and then
+            choose one from the remaining two. Theoretically, as long as you have two decks you are comfortable playing, the third wont matter since you can just choose one of the two.
+          </Typography>
+        </Grid>
+        <Grid xs={12} md={2} />
+      </Grid>
+      <Footer />
     </ThemeProvider>
   )
 }
 
 export default page
 
-// The GM Dueling Club is for those who make it into Grand Masters and reach a certain predetermined rating. For this season in 2023,
-// it is 8100. If you reach that point, you will be invited into the GM Dueling Club.
