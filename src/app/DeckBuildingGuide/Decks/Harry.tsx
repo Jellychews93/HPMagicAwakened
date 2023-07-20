fee726ffc2d7c807fc93091fb0b0208578d5d708
johnnam1121 <johnnam93@gmail.com>
2023-07-19 23:19:45 -0500

    fixed html entities issue

--- src/app/DeckBuildingGuide/Decks/Harry.tsx
+++ src/app/DeckBuildingGuide/Decks/Harry.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from 'react';
 import { Box, Grid, Paper, Typography } from '@mui/material';
 import Image from 'next/image';
 import Two from '../../../../public/assets/DeckBuilding/Two.png';
 
 export default function Harry() {
-  const imageSize = '700'
+  const imageSize = '700';
   return (
     <Box>
       <Typography variant='h3'>
         yEr a WIzaRD, heRRy
       </Typography>
       <Typography variant='h5'>
-        Harry's echo grants damage and healing boost to all spells that are 3 cost or lower. Naturally, we include cheap spells. Your main combo are the following: Oppugno + Broom & Inflatus + Bewitched Snowball. These are the cards that deal the highest damage in Harry's arsenal.<br />
-        Due to the fact that Harry spams a lot of cheap spells, we can benefit from the discount effect of Three Headed Dog. Note that you should ONLY use the dog when its 0 cost. Not when its 3 cost, not when its 2, and not when its 1. Only use at 0 cost.<br />
-        With that in mind, here are a collection of cards that synergise well with Harry, written in the aforementioned three category format:<br />
+        Harry&apos;s echo grants damage and healing boost to all spells that are 3 cost or lower. Naturally, we include cheap spells. Your main combo are the following: Oppugno + Broom & Inflatus + Bewitched Snowball. These are the cards that deal the highest damage in Harry&apos;s arsenal.<br />
+        Due to the fact that Harry spams a lot of cheap spells, we can benefit from the discount effect of Three Headed Dog. Note that you should ONLY use the dog when it&apos;s 0 cost. Not when it&apos;s 3 cost, not when it&apos;s 2, and not when it&apos;s 1. Only use at 0 cost.<br />
+        With that in mind, here are a collection of cards that synergize well with Harry, written in the aforementioned three-category format:<br />
         <br />
         ⭐Central Cards:<br />
         Bewitched Snowball, Oppugno<br /><br />
@@ -26,7 +26,7 @@ export default function Harry() {
         Expelliarmus<br />
         Episkey or Essence of Dittany<br />
         Portkey<br />
-        Prior Incantanto (Note: copied 3 cost cards becoming 4 cost CAN benefit from echo buff. Have fun with double Snowball)<br />
+        Prior Incantanto (Note: copied 3 cost cards becoming 4 cost CAN benefit from the echo buff. Have fun with double Snowball)<br />
         Three Headed Dog (discount)<br />
         Golden Snitch ＊(not yet in global; function includes extra mana for casting and movement cards for kiting)<br />
         Manticore (not recommended, but you could use this if you desperately need more movement)<br />
@@ -36,7 +36,7 @@ export default function Harry() {
         Incendio<br />
         Atmosphere Charm<br />
         Crucio<br />
-        Ideally, Harry echo should not include any of the cards listed in the Techs section. Harry echo demands high precision and a noticeably higher skill level to play well. Using your cheap spells smartly to dispatch swarm of creatures, stupefy to pinball Piertotum Locomotor, breaking dragon eggs with Max Range snowballs, etc. Instead of relying on the tech cards like venom or atmosphere charm.
+        Ideally, Harry echo should not include any of the cards listed in the Techs section. Harry echo demands high precision and a noticeably higher skill level to play well. Using your cheap spells smartly to dispatch swarms of creatures, stupefy to pinball Piertotum Locomotor, breaking dragon eggs with Max Range snowballs, etc. Instead of relying on the tech cards like venom or atmosphere charm.
       </Typography>
       <Grid container sx={{ my: '5vh' }}>
         <Grid item xs={12} md={4}>
@@ -50,17 +50,17 @@ export default function Harry() {
         </Grid>
       </Grid>
       <Typography variant='h5'>
-        Among harry's choice of companions, there are TWO very important cards that you will wish to squeeze into the roster. Luna Lovegood Companion and Prof. McGonagall companion. Unfortuntately, neither Luna companion or Prof. McGonagall are available in global yet. But as they are integral to future Harry echo combos, we will talk about them for a bit.<br />
-        The moment you summon Prof. McGonagall, she will transfigure every unit on the map into a small critter. Transfigured units take extra damage, and are slowed (and unable to attack, if they are creature or companion units). This is essentially a screen wide time-out button.<br />
-        While she is on the field, every time you use a card, she will transfigure a random unit on the field. Which means if there is only the enemy wizard, she will consistently transfigure them, constantly slowing them and let you deal extra damage.<br />
-        After you have cast 8 cards, she will summon a single piertotum locomotor statue.<br />
-        Ideally, you wait until 9 or 10 mana, put down Prof. McGonagall, and spam all your spells in quick succession. You can hide/protect her with Nebulus, prolonging her presence on the field and creating pressure through slow effect/summon statue.<br />
+        Among Harry&apos;s choice of companions, there are TWO very important cards that you will wish to squeeze into the roster. Luna Lovegood Companion and Prof. McGonagall companion. Unfortunately, neither Luna companion nor Prof. McGonagall are available in global yet. But as they are integral to future Harry echo combos, we will talk about them for a bit.<br />
+        The moment you summon Prof. McGonagall, she will transfigure every unit on the map into a small critter. Transfigured units take extra damage and are slowed (and unable to attack if they are creature or companion units). This is essentially a screen-wide time-out button.<br />
+        While she is on the field, every time you use a card, she will transfigure a random unit on the field. Which means if there is only the enemy wizard, she will consistently transfigure them, constantly slowing them and letting you deal extra damage.<br />
+        After you have cast 8 cards, she will summon a single Piertotum Locomotor statue.<br />
+        Ideally, you wait until 9 or 10 mana, put down Prof. McGonagall, and spam all your spells in quick succession. You can hide/protect her with Nebulus, prolonging her presence on the field and creating pressure through the slow effect/summon statue.<br />
         -----<br />
-        While Luna Lovegood companion is on the field, every time you cast a spell, she will summon a random creature of corresponding cost -1. For example, cast a 3 mana spell, she will summon a random 2 cost creature for you. After accumulating a total of 20 mana, she will summon a Thestral, which deals high damage in a small area. Again, protect her with Nebulus, and spam your spells after waiting for 10 or 9 mana. She will summon a swarm of creatures for you, and if enemy doesn't kill her quickly, will summon a hard hitting thestral.<br />
+        While Luna Lovegood companion is on the field, every time you cast a spell, she will summon a random creature of corresponding cost -1. For example, cast a 3 mana spell, she will summon a random 2-cost creature for you. After accumulating a total of 20 mana, she will summon a Thestral, which deals high damage in a small area. Again, protect her with Nebulus and spam your spells after waiting for 10 or 9 mana. She will summon a swarm of creatures for you, and if the enemy doesn't kill her quickly, will summon a hard-hitting thestral.<br />
         But for now....<br />
         In global, we suggest using Lottie, Ron, Hagrid.<br />
         Replace Lottie with Grawp or Frey Twins, or Cassandra.<br />
       </Typography>
     </Box>
-  )
+  );
 }
--- src/app/DeckBuildingGuide/Decks/Hermione.tsx
+++ src/app/DeckBuildingGuide/Decks/Hermione.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Box, Grid, Paper, Typography } from '@mui/material';
 import Image from 'next/image';
 import Three from '../../../../public/assets/DeckBuilding/Three.png';
@@ -7,7 +7,7 @@ import Five from '../../../../public/assets/DeckBuilding/Five.png';
 import Six from '../../../../public/assets/DeckBuilding/Six.png';
 
 export default function Hermione() {
-  const imageSize = '700'
+  const imageSize = '700';
   return (
     <>
       <Typography variant='h3' sx={{}}>
@@ -15,9 +15,9 @@ export default function Hermione() {
       </Typography>
       <Typography variant='h5'>
         It is no exaggeration that Hermy echo has dominated (and continues to dominate) the HPMA meta with her simplistic yet powerful echo effect.<br />
-        Her echo effect involves halving the most expensive card in hand and boosting it's card level by 1 (50% probability for level boost) after playing three spells.<br />
+        Her echo effect involves halving the most expensive card in hand and boosting it&apos;s card level by 1 (50% probability for level boost) after playing three spells.<br />
         And so the usual way to build her is to pick One very expensive spell, and bring a whole bunch of cheap spells to facilitate her echo buff. Some variations pick Two very expensive spells and let the echo effect buff them in rotation.<br />
-        However we will discuss the two most popular hermy deck, the Hermy Thunderstorm, and the Hermy Firework/Whizbang.<br />
+        However, we will discuss the two most popular hermy decks, the Hermy Thunderstorm, and the Hermy Firework/Whizbang.<br />
         I am not kidding when I say that Thunderstorm and Whizbang are almost always banned every month.
         <br /><br />
         ⭐Central Cards:<br />
@@ -55,13 +55,13 @@ export default function Hermione() {
         Sample of Thunder Hermy and Whizbang Hermy.<br />
         Again, Luna and Prof. Mcgonagall are very strong picks for her, but its not available yet.<br />
         Lottie/Grawp/Frey Twins are all worth considering. Cassandra is helpful in dealing with creatures but bad against spells. Use Hermy companion as 1st if you want double thunderstorm.<br />
-        Ron as second companion, or Hermy.<br />
-        Hagrid as third.<br /><br />
+        Ron as the second companion, or Hermy.<br />
+        Hagrid as the third.<br /><br />
         Thunder Hermy brings Portkey? Why Portkey?<br />
         You can throw the boot into enemy nebulus and break the fog. The portkey will grant vision, allowing Thunderstorm to hit things that are hiding inside. <br />
-        Bewitched snowball follows a similar logic. The snowman with the aoe slow effect also grants vision into nebulus, but it is a lot less reliable than portkey since people can just dodge snowball, potentially letting it roll past the nebulus entirely.<br />
-        If you don't find snowball to be a good fit for your deck, replace Snowball for something more useful, like three headed dog.<br />
-        Proof that boot can pierce nebulus<br />
+        Bewitched snowball follows a similar logic. The snowman with the aoe slow effect also grants vision into nebulus, but it is a lot less reliable than portkey since people can just dodge the snowball, potentially letting it roll past the nebulus entirely.<br />
+        If you don't find the snowball to be a good fit for your deck, replace Snowball with something more useful, like the three-headed dog.<br />
+        Proof that the boot can pierce nebulus<br />
       </Typography>
       <Grid container sx={{ my: '5vh' }}>
         <Grid item xs={12} md={4}>
@@ -78,7 +78,7 @@ export default function Hermione() {
         Sample of Whizbang<br />
         Remember that your echo discount occurs after three spells.<br />
         So generally you'll be using: cheap, cheap, cheap spell, then play the buffed expensive spell.<br />
-        For thunderstorm, cheap cheap cheap (example, oppugno into broom, then expulso) to discount the thunderstorm in your hand. Then you play thunderstorm. If enemy has nebulus, you wait for 2 additional mana, play thunderstorm and immediately throw portkey into enemy nebulus. For the sake of optimization, cheap-cheap-portkey-thunderstorm (after waiting for mana to play both portkey and thunderstorm immediately).<br />
+        For thunderstorm, cheap cheap cheap (example, oppugno into broom, then expulso) to discount the thunderstorm in your hand. Then you play thunderstorm. If the enemy has nebulus, you wait for 2 additional mana, play thunderstorm and immediately throw portkey into enemy nebulus. For the sake of optimization, cheap-cheap-portkey-thunderstorm (after waiting for mana to play both portkey and thunderstorm immediately).<br />
         For whizbang, cheap cheap spell, then a control spell, then whizbang. (example: oppugno into broom, then wait 7 mana, incarcerous into discounted 4 mana whizbang).<br />
         Additional footages of Thunder Hermy:
         <Grid container sx={{ my: '5vh' }}>
@@ -100,7 +100,7 @@ export default function Hermione() {
         </Grid>
         <Typography variant='h5'>
           <br />What is Double Thunderstorm? And Triple Thunderstorm?
-          <br />Prior Incantanto allows you to copy a spell you previously used. Through precise control of card rotation, it is possible to play two thunderstorms in quick succession. This is known as the double thunderstorm. To achieve this, the sequence of cards should be:
+          <br />Prior Incantanto allows you to copy a spell you previously used. Through precise control of card rotation, it is possible
           <br />Cheap, cheap, cheap, buff the thunderstorm but don't play it yet.
           <br />Then, cheap, cheap, discounted thunderstorm, into discounted prior incantanto copied thunderstorm.
           <br />The Hermione companion will immediately use an identical spell when you put her down and cast a spell. So Hermy companion is required to achieve triple thunderstorm.
